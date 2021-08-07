c708979f8028133a126d519c261ead1709279396
HigorAln <higor.allan21@gmail.com>
2021-08-07 12:19:54 -0300

    Html e Css

---
+++ arq.js
@@ -0,0 +1,6 @@
+window.onload=function(){
+    document.getElementById('modalFechar').addEventListener('click',FecharModalAgora)
+}
+const FecharModalAgora=()=>{
+    document.getElementById('modalHigor').style.display='none';
+}
